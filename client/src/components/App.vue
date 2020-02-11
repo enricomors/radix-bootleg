5baa559293f4437968f3db76ec9e152f166fa4c0
Enrico Morselli <enrico.morselli@studio.unibo.it>
2020-02-11 12:53:34 +0100

    alcuni cambiamenti provvisori

---
+++ client/src/components/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'app',
+  
+})
+</script>
+
+<style>
+
+</style>
