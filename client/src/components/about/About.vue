b6436dfd6e144cc821e751db278d4f0734cdc234
enricomors <enrico.morselli@studio.unibo.it>
2020-02-20 07:54:18 +0100

    modified about page

--- client/src/components/about/About.vue
+++ client/src/components/about/About.vue
@@ -2,8 +2,20 @@
   <div class="section">
     <h1 class="title">About</h1>
     <p>
-      This is a project showcasing the different things you can do with the
-      <a href="https://github.com/radixdlt/radixdlt-js">Radix DLT JavaScript client library</a>.
+      This is an application inspired by <a href="https://consensys.net/web3studio/bootleg">Bootleg Project</a> 
+      by Web3Studio, built using the <a href="https://github.com/radixdlt/radixdlt-js">Radix DLT JavaScript client library</a>.
+    </p>
+    <br>
+    <p>
+      The GitHub repo for this project is available <a href="https://github.com/enricomors/radix-bootleg">here</a>.
+      You can also check-out the <a href="https://github.com/enricomors/radix-bootleg/wiki">project wiki</a> for
+      further informations.
+    </p>
+    <br>
+    <p>
+      This front-end is based on the Radix DLT JS Showcase project, that you can find
+      <a href="https://github.com/radixdlt/radixdlt-js-showcase">here</a>. Check out also the website of
+      <a href="https://www.radixdlt.com/">Radix DLT</a>.
     </p>
   </div>
 </template>
